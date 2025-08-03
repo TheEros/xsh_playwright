d097c857b6be4b5341d809348c9513191df8cd35
TheEros <5605058+zhou_linjun@user.noreply.gitee.com>
2025-08-03 16:00:06 +0800

    fix some

--- selenium_test.py
+++ selenium_test.py
@@ -1,6 +1,8 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-import time
+from bs4 import BeautifulSoup
 
 
 def screenshot_note_with_cookies(note_url):
@@ -48,6 +50,22 @@ def screenshot_note_with_cookies(note_url):
             screenshot_path = "./screenshots/{}_note_screenshot.png".format(i)
             driver.save_screenshot(screenshot_path)
             print(f"截图已保存到: {screenshot_path}")
+            page_source = driver.page_source
+
+            # 使用BeautifulSoup解析
+            soup = BeautifulSoup(page_source, "html.parser")
+            if soup.find("title").text == "小红书 - 你访问的页面不见了":
+                print(f"无法访问: {url}")
+                break
+            title = soup.find("meta", attrs={"name": "og:title"})["content"]
+            note_comment = soup.find("meta", attrs={"name": "og:xhs:note_comment"})[
+                "content"
+            ]
+            note_like = soup.find("meta", attrs={"name": "og:xhs:note_like"})["content"]
+            note_collect = soup.find("meta", attrs={"name": "og:xhs:note_collect"})[
+                "content"
+            ]
+            print(title, url, note_like, note_collect, note_comment)
 
     finally:
         driver.quit()
@@ -55,42 +73,6 @@ def screenshot_note_with_cookies(note_url):
 
 # 使用示例
 note_urls = [
-    "http://xhslink.com/m/8Y3ukK2cahd",
-    "http://xhslink.com/m/5DiLqjqyT8u",
-    "http://xhslink.com/m/3cTzDKWiBEt",
-    "http://xhslink.com/m/8DawsZPEqAN",
-    "http://xhslink.com/m/7B4ze9YPBXS",
-    "http://xhslink.com/m/1AM72qGlPat",
-    "http://xhslink.com/m/12cQ9koePfn",
-    "http://xhslink.com/m/4NI9aJFNxho",
-    "http://xhslink.com/m/54TtcvuHDq3",
-    "http://xhslink.com/m/3oDa3n4jr2U",
-    "http://xhslink.com/m/7f093d9HzUv",
-    "http://xhslink.com/m/AZ9P8f3YDCn",
-    "http://xhslink.com/m/5ak2mTjNtBE",
-    "http://xhslink.com/m/AYoqq4Qe80q",
-    "http://xhslink.com/m/7elaW6cQSFf",
-    "http://xhslink.com/m/4qa9RwBf9vO",
-    "http://xhslink.com/m/7LpmTyea5H7",
-    "http://xhslink.com/m/6aAU8pV9RkX",
-    "http://xhslink.com/m/9YKgADzThbf",
-    "https://www.xiaohongshu.com/discovery/item/688ddf5d000000002303b8dd?app_platform=android&ignoreEngage=true&app_version=8.91.4&share_from_user_hidden=true&xsec_source=app_share&type=video&xsec_token=CBVlx-ZtVyMHDebKo9UD9TZld0qSu1vvG4CcYVJ4FFepY%3D&author_share=1&xhsshare=CopyLink&shareRedId=ODw5QTw6PUE2NzUyOTgwNjZHOTk6OkdB&apptime=1754176575&share_id=3baee04c0bc244ab857d77c13052bb3f&share_channel=copy_link",
-    "http://xhslink.com/m/fjL8w53m73",
-    "http://xhslink.com/m/xZe3QT1lRc",
-    "http://xhslink.com/m/9tI8QnvlC2e",
     "http://xhslink.com/m/AYKY5UKt5JM",
-    "http://xhslink.com/m/1UkGcnOPynF",
-    "http://xhslink.com/m/AxXTSOG3Ptr",
-    "http://xhslink.com/m/7AFl33ccXgq",
-    "http://xhslink.com/m/9u9Dcf4zPU4",
-    "http://xhslink.com/m/7TFJh54aifM",
-    "http://xhslink.com/n/4nFwt7T0PJx",
-    "http://xhslink.com/m/4OXjPturJPR",
-    "http://xhslink.com/m/dEOovFBrjD",
-    "http://xhslink.com/m/AsUhTlarupt",
-    "http://xhslink.com/m/7vGru1SZwR7",
-    "http://xhslink.com/m/95w9BmuZzAb",
-    "http://xhslink.com/m/26DHbnXr647",
-    "http://xhslink.com/m/3gsI4tzeVW6",
 ]  # 替换为实际笔记URL
 screenshot_note_with_cookies(note_urls)
